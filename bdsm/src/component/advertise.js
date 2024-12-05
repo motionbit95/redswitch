cd9c384dd87f30958d833fd40f721240323e16ed
motionbit95 <motionbit.dev@gmail.com>
2024-12-05 23:21:16 +0900

    temp

--- bdsm/src/component/advertise.js
+++ bdsm/src/component/advertise.js
@@ -7,7 +7,9 @@ const AdCarousel = ({ position, ...props }) => {
   useEffect(() => {
     const fetchAdvertiseBanner = async () => {
       try {
-        const response = await fetch("http://localhost:8080/advertisements");
+        const response = await fetch(
+          `${process.env.REACT_APP_SERVER_URL}/advertisements`
+        );
         const result = await response.json();
         setImages(result.data);
       } catch (error) {
