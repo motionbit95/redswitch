6d1d992a4caf4e913c4753332f828b180c2d9b94
wlsdud9973 <jyp260399@gmail.com>
2025-01-07 14:24:19 +0900

    설치지점 이미지 및 로고 불러오기

--- landing/src/page/Pages.js
+++ landing/src/page/Pages.js
@@ -4,356 +4,80 @@ import Description from "../component/description";
 import { useMediaQuery } from "react-responsive";
 import { Button, Col, Image, Row, Space } from "antd";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
-
-// export function Main(props) {
-//   const { size } = props;
-//   return (
-//     <Row
-//       gutter={[16, 16]}
-//       style={{
-//         overflow: "hidden",
-//         display: "flex",
-//         flex: 1,
-//         height: "100vh",
-//         paddingTop: "80px",
-//         paddingInline:
-//           size === "mobile" ? "20px" : size === "tablet" ? "48px" : "72px",
-//       }}
-//     >
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 9 : 12}
-//         style={{ padding: "20px", zIndex: 999 }}
-//       >
-//         <Space direction="vertical" size={"large"}>
-//           <Space direction="vertical">
-//             <MainDescription
-//               size={size}
-//               description="숙박업소 비대면 어덜트토이 플랫폼"
-//             />
-//             <Space direction="vertical" style={{ gap: "0px" }}>
-//               <MainFontBox size={size} title="RED" color="#DA0A18" />
-//               <MainFontBox size={size} title="SWITCH" color="white" />
-//             </Space>
-//             <MainDescription
-//               size={size}
-//               description="가장 필요한 순간, 간편 주문, 즉시 도착."
-//             />
-//           </Space>
-//         </Space>
-//       </Col>
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 15 : 12}
-//         style={{
-//           padding: "20px",
-//           alignItems: "flex-end",
-//           justifyContent: "flex-end",
-//           display: "flex",
-//         }}
-//       >
-//         <Space direction="vertical">
-//           <Image
-//             preview={false}
-//             src={require("../asset/section/IMAGE_2.png")}
-//           />
-//         </Space>
-//       </Col>
-//     </Row>
-//   );
-// }
-
-// export function Intro2(props) {
-//   const { size } = props;
-//   return (
-//     <Row
-//       gutter={[16, 16]}
-//       style={{
-//         overflow: "hidden",
-//         height: "100vh",
-//         display: "flex",
-//         flex: 1,
-//         paddingTop: "80px",
-//         paddingInline:
-//           size === "mobile" ? "20px" : size === "tablet" ? "48px" : "72px",
-//       }}
-//     >
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 9 : 12}
-//         style={{ padding: "20px", zIndex: 999 }}
-//       >
-//         <Space direction="vertical" size={"large"}>
-//           <TextBox
-//             size={size}
-//             title="Adult Toy"
-//             description="성인용품 사용해보신적 있나요?"
-//           />
-//           <Space direction="vertical">
-//             <Description
-//               size={size}
-//               description={`대답하기 부끄러운 것은 저 뿐만이 아니겠죠?`}
-//             />
-//             <div>
-//               <Description
-//                 size={size}
-//                 description={`우리는 성적인 존재가 아닌 척 하는 데에`}
-//               />
-//               <Description
-//                 size={size}
-//                 description={`너무 오랜 시간을 보냈습니다.`}
-//               />
-//             </div>
-//           </Space>
-//         </Space>
-//       </Col>
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 15 : 12}
-//         style={{
-//           padding: "20px",
-//           alignItems: "flex-end",
-//           justifyContent: "flex-end",
-//           display: "flex",
-//         }}
-//       >
-//         <Space direction="vertical">
-//           <Image
-//             preview={false}
-//             src={require("../asset/section/IMAGE_2.png")}
-//           />
-//         </Space>
-//       </Col>
-//     </Row>
-//   );
-// }
-
-// export function Intro3(props) {
-//   const { size } = props;
-//   return (
-//     <Row
-//       gutter={[16, 16]}
-//       style={{
-//         overflow: "hidden",
-//         height: "100vh",
-//         display: "flex",
-//         flex: 1,
-//         paddingTop: "80px",
-//         paddingInline:
-//           size === "mobile" ? "20px" : size === "tablet" ? "48px" : "72px",
-//       }}
-//     >
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 9 : 12}
-//         style={{ padding: "20px", zIndex: 999 }}
-//       >
-//         <Space direction="vertical" size={"large"}>
-//           <TextBox
-//             size={size}
-//             title="ADLT MARKET"
-//             description="성인용품 시장 5조원 시대"
-//           />
-//           <div>
-//             <Description
-//               size={size}
-//               description={`하지만 더 나은 섹스를 위한 어들트토이를 갖기까지`}
-//             />
-//             <Description
-//               size={size}
-//               description={`우리는 부끄러운 마음과 걱정이 먼저 앞서는걸요.`}
-//             />
-//           </div>
-//         </Space>
-//       </Col>
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 15 : 12}
-//         style={{
-//           padding: "20px",
-//           alignItems: "flex-end",
-//           justifyContent: "flex-end",
-//           display: "flex",
-//         }}
-//       >
-//         <Space direction="vertical">
-//           <Image
-//             preview={false}
-//             src={require("../asset/section/IMAGE_2.png")}
-//           />
-//         </Space>
-//       </Col>
-//     </Row>
-//   );
-// }
-
-// export function Service1(props) {
-//   const { size } = props;
-//   return (
-//     <Row
-//       gutter={[16, 16]}
-//       style={{
-//         overflow: "hidden",
-//         height: "100vh",
-//         display: "flex",
-//         flex: 1,
-//         paddingTop: "80px",
-//         paddingInline:
-//           size === "mobile" ? "20px" : size === "tablet" ? "48px" : "72px",
-//       }}
-//     >
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 9 : 12}
-//         style={{ padding: "20px", zIndex: 999 }}
-//       >
-//         <TextBox
-//           size={size}
-//           title="SWITCH ON!"
-//           description="가장 필요한 순간, 간편 주문, 즉시도착!"
-//         />
-//       </Col>
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 15 : 12}
-//         style={{
-//           padding: "20px",
-//           alignItems: "flex-end",
-//           justifyContent: "flex-end",
-//           display: "flex",
-//         }}
-//       >
-//         <Space direction="vertical">
-//           <Image
-//             preview={false}
-//             src={require("../asset/section/IMAGE_2.png")}
-//           />
-//         </Space>
-//       </Col>
-//     </Row>
-//   );
-// }
-
-// export function Service2(props) {
-//   const { size } = props;
-//   return (
-//     <Row
-//       gutter={[16, 16]}
-//       style={{
-//         overflow: "hidden",
-//         height: "100vh",
-//         display: "flex",
-//         flex: 1,
-//         paddingTop: "80px",
-//         paddingInline:
-//           size === "mobile" ? "20px" : size === "tablet" ? "48px" : "72px",
-//       }}
-//     >
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 9 : 12}
-//         style={{ padding: "20px", zIndex: 999 }}
-//       >
-//         <Space direction="vertical" size={"large"}>
-//           <TextBox
-//             size={size}
-//             title="SEX MBTI"
-//             description="너 섹비티아이 뭐야?"
-//           />
-//           <Space direction="vertical">
-//             <Description
-//               size={size}
-//               description={`MBTI는 알지만, 섹스 퍼스널리티는 모른다구요?`}
-//             />
-//             <div>
-//               <Description
-//                 size={size}
-//                 description={`MBTI처럼 섹스 퍼스널리티에도`}
-//               />
-//               <Description
-//                 size={size}
-//                 description={`22가지의 다양한 성향이 있어요.`}
-//               />
-//             </div>
-//           </Space>
-//         </Space>
-//       </Col>
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 15 : 12}
-//         style={{
-//           padding: "20px",
-//           alignItems: "flex-end",
-//           justifyContent: "flex-end",
-//           display: "flex",
-//         }}
-//       >
-//         <Space direction="vertical">
-//           <Image
-//             preview={false}
-//             src={require("../asset/section/IMAGE_2.png")}
-//           />
-//         </Space>
-//       </Col>
-//     </Row>
-//   );
-// }
-
-// export function Service3(props) {
-//   const { size } = props;
-//   return (
-//     <Row
-//       gutter={[16, 16]}
-//       style={{
-//         overflow: "hidden",
-//         height: "100vh",
-//         display: "flex",
-//         flex: 1,
-//         paddingTop: "80px",
-//         paddingInline:
-//           size === "mobile" ? "20px" : size === "tablet" ? "48px" : "72px",
-//       }}
-//     >
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 9 : 12}
-//         style={{ padding: "20px", zIndex: 999 }}
-//       >
-//         <Space direction="vertical" size={"large"}>
-//           <TextBox
-//             size={size}
-//             title="BDSM TEST"
-//             description="무료 SEX MBTI 테스트"
-//           />
-//           <Space direction="vertical">
-//             <Description
-//               size={size}
-//               description={`간단한 테스트로 성향을 공유해보세요.`}
-//             />
-//             <div>
-//               <Description
-//                 size={size}
-//                 description={`상대의 성향과 서로에게 필요한 제품을 알 수 있어요.`}
-//               />
-//               <Description
-//                 size={size}
-//                 description={`지피지기백전백승! 오늘 사랑이 더 깊어지지 않을까요?`}
-//               />
-//             </div>
-//           </Space>
-//         </Space>
-//       </Col>
-//       <Col
-//         span={size === "mobile" ? 24 : size === "tablet" ? 15 : 12}
-//         style={{
-//           padding: "20px",
-//           alignItems: "flex-end",
-//           justifyContent: "flex-end",
-//           display: "flex",
-//         }}
-//       >
-//         <Space direction="vertical">
-//           <Image
-//             preview={false}
-//             src={require("../asset/section/IMAGE_2.png")}
-//           />
-//         </Space>
-//       </Col>
-//     </Row>
-//   );
-// }
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 export function Customer(props) {
   const { size } = props;
+  const [spot, setSpot] = useState([]);
+  const [logoIndex, setLogoIndex] = useState(0);
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const handleNext = () => {
+    // 로고의 경우, index가 마지막에 도달하면 처음으로 돌아가도록 설정
+    if (logoIndex < spot.length - 3) {
+      setLogoIndex(logoIndex + 1);
+    } else {
+      setLogoIndex(0); // 끝에 도달하면 첫 번째로 돌아가기
+    }
+  };
+
+  const handlePrev = () => {
+    // 로고가 처음에 도달하면 마지막으로 돌아가도록 설정
+    if (logoIndex > 0) {
+      setLogoIndex(logoIndex - 1);
+    } else {
+      setLogoIndex(spot.length - 3); // 처음에 도달하면 마지막으로 돌아가기
+    }
+  };
+
+  const handleNextImage = () => {
+    // 이미지의 경우, index가 마지막에 도달하면 처음으로 돌아가도록 설정
+    if (imageIndex < spot.length - 2) {
+      setImageIndex(imageIndex + 1);
+    } else {
+      setImageIndex(0); // 끝에 도달하면 첫 번째로 돌아가기
+    }
+  };
+
+  const handlePrevImage = () => {
+    // 이미지가 처음에 도달하면 마지막으로 돌아가도록 설정
+    if (imageIndex > 0) {
+      setImageIndex(imageIndex - 1);
+    } else {
+      setImageIndex(spot.length - 2); // 처음에 도달하면 마지막으로 돌아가기
+    }
+  };
+
+  // 로고 3개 표시
+  const visibleLogos = spot.slice(logoIndex, logoIndex + 3);
+
+  // 이미지 2개 표시
+  const visibleImages = spot.slice(imageIndex, imageIndex + 2);
+
+  useEffect(() => {
+    fetchSpot();
+  }, []);
+
+  const fetchSpot = async () => {
+    try {
+      const response = await axios
+        .get(
+          `https://port-0-redswitch-lxwmkqxz2d25ae69.sel5.cloudtype.app/spots`
+        )
+        .then((response) => {
+          console.log("Spot fetched:", response.data);
+          setSpot(response.data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <Row
       gutter={[16, 16]}
@@ -404,24 +128,27 @@ export function Customer(props) {
                 display: "flex",
               }}
             >
-              <Button style={{ borderRadius: "50%" }} icon={<LeftOutlined />} />
+              <Button
+                style={{ borderRadius: "50%" }}
+                icon={<LeftOutlined />}
+                onClick={handlePrev}
+              />
               <Space style={{ padding: size === "mobile" ? 0 : "20px" }}>
-                {["1", "2", "3"].map((item) => {
-                  return (
-                    <div
-                      style={{
-                        width: size === "mobile" ? "50px" : "100px",
-                        height: size === "mobile" ? "50px" : "100px",
-                        backgroundColor: "#f1f1f1",
-                        borderRadius: "50%",
-                      }}
-                    ></div>
-                  );
-                })}
+                {visibleLogos.map((spot, idx) => (
+                  <Image
+                    style={{
+                      width: size === "mobile" ? "50px" : "100px",
+                      height: size === "mobile" ? "50px" : "100px",
+                      borderRadius: "50%",
+                    }}
+                    src={spot.spot_logo}
+                  />
+                ))}
               </Space>
               <Button
                 style={{ borderRadius: "50%" }}
                 icon={<RightOutlined />}
+                onClick={handleNext}
               />
             </Space>
             <Space
@@ -429,24 +156,29 @@ export function Customer(props) {
                 display: "flex",
               }}
             >
-              <Button style={{ borderRadius: "50%" }} icon={<LeftOutlined />} />
+              <Button
+                style={{ borderRadius: "50%" }}
+                icon={<LeftOutlined />}
+                onClick={handlePrevImage}
+              />
               <Space style={{ padding: size === "mobile" ? 0 : "20px" }}>
-                {["1", "2", "3"].map((item) => {
-                  return (
-                    <div
-                      style={{
-                        width: size === "mobile" ? "80px" : "120px",
-                        height: size === "mobile" ? "80px" : "120px",
-                        backgroundColor: "#f1f1f1",
-                        borderRadius: "20%",
-                      }}
-                    ></div>
-                  );
-                })}
+                {visibleImages.map((spot, idx) => (
+                  <Image
+                    style={{
+                      width: size === "mobile" ? "80px" : "120px",
+                      height: size === "mobile" ? "80px" : "120px",
+                      backgroundColor: "#f1f1f1",
+                      borderRadius: "20%",
+                      border: "1px solid #f1f1f1",
+                    }}
+                    src={spot.spot_image}
+                  />
+                ))}
               </Space>
               <Button
                 style={{ borderRadius: "50%" }}
                 icon={<RightOutlined />}
+                onClick={handleNextImage}
               ></Button>
             </Space>
           </Space>
