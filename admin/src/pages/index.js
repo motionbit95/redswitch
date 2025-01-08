0999e726b73cc0dd1fbb9509661d916d390a5da7
wlsdud9973 <jyp260399@gmail.com>
2025-01-08 18:17:15 +0900

    권한에 따른 menu 설정 컴포넌트화

--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -22,55 +22,153 @@ import PaymentSummaryByBranch from "./sales/branch";
 
 function App(props) {
   const { currentUser } = props;
+
+  // 권한별 라우트 구성
+  const renderRoutesByPermission = () => {
+    switch (currentUser?.permission) {
+      case "1": // 최고관리자
+        return (
+          <>
+            <Route
+              path="/dashboard"
+              element={<Main currentUser={currentUser} />}
+            />
+            <Route path="/admin/account" element={<Account />} />
+            <Route path="/admin/spot" element={<Spot />} />
+            <Route path="/branch/branch" element={<Branch />} />
+            <Route path="/provider/provider" element={<Provider />} />
+            <Route path="/provider/post" element={<FranchisePost />} />
+            <Route path="/bdsm/questions" element={<BDSMQuestions />} />
+            <Route path="/bdsm/results" element={<BDSMResults />} />
+            <Route path="/bdsm/advertise" element={<BDSMAdvertise />} />
+            <Route path="/bdsm/trend" element={<BDSMStatistics />} />
+            <Route
+              path="/product/material"
+              element={<Material currentUser={currentUser} />}
+            />
+            <Route
+              path="/product/product"
+              element={<Product currentUser={currentUser} />}
+            />
+            <Route
+              path="/product/inventory"
+              element={<Inventory currentUser={currentUser} />}
+            />
+            <Route
+              path="/product/purchase_order"
+              element={<Purchase_order />}
+            />
+            <Route
+              path="/order"
+              element={<Order currentUser={currentUser} />}
+            />
+            <Route
+              path="/post/notification"
+              element={<NoticeBoard currentUser={currentUser} />}
+            />
+            <Route
+              path="/post/inquiry"
+              element={<InquiryBoard currentUser={currentUser} />}
+            />
+            <Route
+              path="/sales/sales"
+              element={<PaymentSummary currentUser={currentUser} />}
+            />
+            <Route path="/sales/branch" element={<PaymentSummaryByBranch />} />
+          </>
+        );
+      case "2": // 지사관리자
+        return (
+          <>
+            <Route
+              path="/dashboard"
+              element={<Main currentUser={currentUser} />}
+            />
+            <Route path="/branch/branch" element={<Branch />} />
+            <Route path="/provider/provider" element={<Provider />} />
+            <Route path="/provider/post" element={<FranchisePost />} />
+            <Route
+              path="/product/material"
+              element={<Material currentUser={currentUser} />}
+            />
+            <Route
+              path="/product/product"
+              element={<Product currentUser={currentUser} />}
+            />
+            <Route
+              path="/product/inventory"
+              element={<Inventory currentUser={currentUser} />}
+            />
+            <Route
+              path="/product/purchase_order"
+              element={<Purchase_order />}
+            />
+            <Route
+              path="/order"
+              element={<Order currentUser={currentUser} />}
+            />
+            <Route
+              path="/post/notification"
+              element={<NoticeBoard currentUser={currentUser} />}
+            />
+            <Route
+              path="/post/inquiry"
+              element={<InquiryBoard currentUser={currentUser} />}
+            />
+            <Route
+              path="/sales/sales"
+              element={<PaymentSummary currentUser={currentUser} />}
+            />
+            <Route path="/sales/branch" element={<PaymentSummaryByBranch />} />
+          </>
+        );
+      case "3": // 지점관리자
+        return (
+          <>
+            <Route
+              path="/dashboard"
+              element={<Main currentUser={currentUser} />}
+            />
+            <Route
+              path="/product/product"
+              element={<Product currentUser={currentUser} />}
+            />
+            <Route
+              path="/product/inventory"
+              element={<Inventory currentUser={currentUser} />}
+            />
+            <Route
+              path="/product/purchase_order"
+              element={<Purchase_order />}
+            />
+            <Route
+              path="/order"
+              element={<Order currentUser={currentUser} />}
+            />
+            <Route
+              path="/post/notification"
+              element={<NoticeBoard currentUser={currentUser} />}
+            />
+            <Route
+              path="/post/inquiry"
+              element={<InquiryBoard currentUser={currentUser} />}
+            />
+            <Route
+              path="/sales/sales"
+              element={<PaymentSummary currentUser={currentUser} />}
+            />
+            <Route path="/sales/branch" element={<PaymentSummaryByBranch />} />
+          </>
+        );
+      default: // 권한이 없을 경우
+        return <Route path="*" element={<div>권한이 없습니다.</div>} />;
+    }
+  };
+
   return (
     <div>
       {/* 페이지 라우팅 */}
-      <Routes>
-        <Route path="/dashboard" element={<Main currentUser={currentUser} />} />
-        <Route path="/admin/account" element={<Account />} />
-        <Route path="/admin/spot" element={<Spot />} />
-
-        <Route path="/branch/branch" element={<Branch />} />
-
-        <Route path="/provider/provider" element={<Provider />} />
-        <Route path="/provider/post" element={<FranchisePost />} />
-
-        <Route path="/bdsm/questions" element={<BDSMQuestions />} />
-        <Route path="/bdsm/results" element={<BDSMResults />} />
-        <Route path="/bdsm/advertise" element={<BDSMAdvertise />} />
-        <Route path="/bdsm/trend" element={<BDSMStatistics />} />
-
-        <Route
-          path="/product/material"
-          element={<Material currentUser={currentUser} />}
-        />
-        <Route
-          path="/product/product"
-          element={<Product currentUser={currentUser} />}
-        />
-        <Route
-          path="/product/inventory"
-          element={<Inventory currentUser={currentUser} />}
-        />
-        <Route path="/product/purchase_order" element={<Purchase_order />} />
-
-        <Route path="/order" element={<Order currentUser={currentUser} />} />
-
-        <Route
-          path="/post/notification"
-          element={<NoticeBoard currentUser={currentUser} />}
-        />
-        <Route
-          path="/post/inquiry"
-          element={<InquiryBoard currentUser={currentUser} />}
-        />
-
-        <Route
-          path="/sales/sales"
-          element={<PaymentSummary currentUser={currentUser} />}
-        />
-        <Route path="/sales/branch" element={<PaymentSummaryByBranch />} />
-      </Routes>
+      <Routes>{renderRoutesByPermission()}</Routes>
     </div>
   );
 }
